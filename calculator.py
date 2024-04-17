8b7049416242ef2b83c5ffc125164982b0014f1f
Flivian91 <fliviandev@gmail.com>
2024-04-17 10:55:20 +0300

    first commit

---
+++ calculator.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+
+
+class Calculator:
+    def __init__(self):
+        self.root = tk.Tk()
+
+
+
+
+    def run(self):
+        self.root.mainloop()
+
+if __name__ == "__main__":
+    root.
